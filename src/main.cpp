0196529fd338235e0fa00d828657ee4747da68ad
gmh5225 <2315157@qq.com>
2023-11-07 17:30:20 +0800

    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <d3d9.h>
+
+#pragma comment(lib, "d3d9.lib")
 
 int
 main()
 {
+    volatile auto x = Direct3DCreate9(2);
+
     std::cout << "Hello from cmkr!\n";
     return 0;
 }
