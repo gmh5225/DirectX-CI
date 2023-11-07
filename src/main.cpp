0bddf4458289b9862b252bf9ed1dcda8864822bc
gmh5225 <2315157@qq.com>
2023-11-07 18:04:21 +0800

    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 #include <d3d9.h>
+#include <d3dx9.h>
+#include <Windows.h>
 
 #pragma comment(lib, "d3d9.lib")
+#pragma comment(lib, "d3dx9.lib")
+
+D3DPRESENT_PARAMETERS g_d3dpp = {};
+LPDIRECT3DDEVICE9 g_pd3dDevice = NULL;
 
 int
 main()
 {
-    volatile auto x = Direct3DCreate9(2);
+    auto g_pD3D = Direct3DCreate9(D3D_SDK_VERSION);
+
+    // Create the D3DDevice
+    ZeroMemory(&g_d3dpp, sizeof(g_d3dpp));
+    g_d3dpp.Windowed = TRUE;
+    g_d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+    g_d3dpp.BackBufferFormat = D3DFMT_UNKNOWN; // Need to use an explicit format with alpha if needing
+                                               // per-pixel alpha composition.
+    g_d3dpp.EnableAutoDepthStencil = TRUE;
+    g_d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
+    g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_ONE; // Present with vsync
+    // g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;   // Present
+    // without vsync, maximum unthrottled framerate
+    if (g_pD3D->CreateDevice(
+            D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, NULL, D3DCREATE_HARDWARE_VERTEXPROCESSING, &g_d3dpp, &g_pd3dDevice) < 0)
+        return false;
 
     std::cout << "Hello from cmkr!\n";
     return 0;
